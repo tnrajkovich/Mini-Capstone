46cdda3e801acbeefe3c61c818629af3032363b3
Tom Rajkovich <tnrajkovich@gmail.com>
2019-06-25 19:16:42 -0500

    Added route to display JSON for all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+  def all_products_method
+    @products = Product.all
+    render "products.json.jb"
+  end
+
+  def one_product_method
+    @product = Product.first
+    render "products.json.jb"
+  end
 end
