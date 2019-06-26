328abc187c5a3027a3200bdbff298db88d3b4b80
Tom Rajkovich <tnrajkovich@gmail.com>
2019-06-25 19:24:28 -0500

    Added to routes.rb

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,8 +4,8 @@ class Api::ProductsController < ApplicationController
     render "products.json.jb"
   end
 
-  def one_product_method
+  def first_product_method
     @product = Product.first
-    render "products.json.jb"
+    render "first_product.json.jb"
   end
 end
