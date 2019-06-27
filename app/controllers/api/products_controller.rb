178c62c0987db30c036d76979cf62d121eb3eaa6
Tom Rajkovich <tnrajkovich@gmail.com>
2019-06-27 18:32:35 -0500

    Added patch update method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -20,4 +20,15 @@ class Api::ProductsController < ApplicationController
     @product.save
     render "show.json.jb"
   end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.id = params["id"] || @product.id
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
 end
