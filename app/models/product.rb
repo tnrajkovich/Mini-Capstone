8350c10f9856b480c97bec7009307a5fc5ede184
Tom Rajkovich <tnrajkovich@gmail.com>
2019-07-01 19:18:28 -0500

    Added paths in controller for validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 10..500 }
+
   def is_discounted?
     if price < 500000
       return true
